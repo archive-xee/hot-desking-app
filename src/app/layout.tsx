9ac760ab319aa45da355d53aff4be1a2eed8856d
Wontae Choi <>
2024-02-04 15:58:45 +0900

    [Add] Pretendard 폰트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
+import localFont from "next/font/local"
 import "@/src/app/globals.css"
+
+const PRETENDARD_FONT = localFont({ src: "../static/fonts/Pretendard.woff2" })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={PRETENDARD_FONT.className}>
       <body>{children}</body>
     </html>
   )
