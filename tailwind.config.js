65e88ffc1b8d876ccb199f7c80aaacfc75f9d922
Wontae Choi <>
2024-02-04 14:57:12 +0900

    [Add] Tailwind.config에 Color 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    colors: {
+      'blue': '#003358',
+      'teal': '#00756f',
+      'purple': '#410268',
+      'yellow': '#ffd900',
+      'white': '#fafaff',
+      'black': '#3c3c3c',
+    },
   },
   plugins: [],
 }
