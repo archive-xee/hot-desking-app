22b7fcd0c636916cdfbc7dcf5b08081603bf3002
Wontae Choi <>
2024-02-06 13:11:07 +0900

    [Add] RootLayout 반응형 + 배경색

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,9 @@ const PRETENDARD_FONT = localFont({ src: "../static/fonts/Pretendard.woff2" })
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={PRETENDARD_FONT.className}>
-      <body>{children}</body>
+      <body>
+        <div className="container mx-auto h-lvh bg-white-100">{children}</div>
+      </body>
     </html>
   )
 }
