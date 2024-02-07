30dcc42dd1a2525a52e250872c93941c8d430142
Wontae Choi <>
2024-02-07 18:46:01 +0900

    [Update] 헤더에 내 이용권, 로그아웃 버튼 추가, 로고에  Root 링크 (/) 추가

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -3,17 +3,29 @@ import Link from "next/link"
 
 export default function Header() {
   return (
-    <div className="container bg-white-300 px-2">
-      <div className="flew-row flex items-center">
-        <div className="box-border py-2 pr-2">
-          <Image src="/images/app_logo.png" alt="궁극의 창작공간" width="32" height="32" />
-        </div>
-        <h1 className="text-lg font-bold">궁극의 창작공간</h1>
-        <div className="grow"></div>
-        <Link href="/signin" className="mr-2 hover:underline">
-          로그인
+    <div className="container flex flex-row items-center bg-white-300 px-2">
+      <Link className="inline-block" href="/">
+        <Image
+          className="box-border py-2 pr-2"
+          src="/images/app_logo.png"
+          alt="궁극의 창작공간"
+          width="36"
+          height="36"
+        />
+      </Link>
+      <h1 className="text-lg font-bold">궁극의 창작공간</h1>
+      <div className="grow"></div>
+      <div className="flex flex-row gap-2">
+        <Link href="/user/ticket" className="hover:underline">
+          내 이용권
         </Link>
         <Link href="/signup" className="hover:underline">
+          로그아웃
+        </Link>
+        <Link href="/signin" className="hover:underline">
+          로그인
+        </Link>
+        <Link href="/signoff" className="hover:underline">
           회원가입
         </Link>
       </div>
