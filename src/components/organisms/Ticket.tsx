369a50ed7dbd9584b9189446f00af09a575dc525
Wontae Choi <>
2024-02-07 12:03:27 +0900

    [Add] 티켓 구매 페이지 플레이스홀더 + TicketForSale

--- src/components/organisms/Ticket.tsx
+++ src/components/organisms/Ticket.tsx
@@ -1,15 +1,14 @@
 import Image from "next/image"
-import type { Ticket, TicketType } from "@/src/models/ticket"
+import type { Ticket } from "@/src/models/ticket"
 import { Bookable } from "@/src/models/ticket"
 
 const TicketForSale = (props: { ticket: Ticket }) => {
   const { ticket } = props
-  const ticketTypeKorean = getTicketTypeKorean(ticket.type)
   const bookableKorean = getBookableKorean(ticket.bookable)
 
   const borderColors: { [key in Bookable]: string } = {
     seat: "border-blue-100",
-    meetingroom: "border-yellow-100",
+    meetingroom: "border-yellow-500",
     rentbox: "border-teal-100",
     locker: "border-purple-100",
   }
@@ -22,48 +21,29 @@ const TicketForSale = (props: { ticket: Ticket }) => {
   }
 
   return (
-    <div
-      className={`flex ${borderColors[ticket.bookable]} flex-row overflow-hidden rounded-lg border border-solid bg-white-300`}
-    >
+    <div className={`flex ${borderColors[ticket.bookable]} flex-row rounded-lg border border-solid bg-white-300`}>
       <div
-        className={`flex ${bookableColors[ticket.bookable]} size-20 flex-col items-center justify-center gap-1 bg-black-300`}
+        className={`flex ${bookableColors[ticket.bookable]} size-20 flex-col items-center justify-center gap-1 rounded-l-lg bg-black-300`}
       >
-        <Image src={`/icons/ticket/${ticket.type}.png`} alt="이것도 해야겠네" width="24" height="24"></Image>
-        <p className="text-white-100">{ticketTypeKorean}권</p>
+        <Image src={`/icons/bookable/${ticket.bookable}.png`} alt="이것도 해야겠네" width="24" height="24"></Image>
+        <p className="text-white-100">{bookableKorean}</p>
       </div>
       <div className="flex grow flex-row justify-between pl-2">
         <div className="flex flex-col justify-center">
-          <p>
-            <span className="mr-1 font-bold">{bookableKorean}</span>
-            90일
-          </p>
-          <p>유효기간 (2024-01-02 ~ 2024-04-02)</p>
+          <p className="font-bold">90일</p>
         </div>
         <div className="flex items-center gap-4">
           <div>
             <p className="font-bold">남은 수량</p>
             <p className="text-center">8개</p>
           </div>
-          <div className="flex size-20 items-center justify-center bg-white-700">
+          <div className="flex size-20 items-center justify-center rounded-r-lg bg-white-700">
             <p className="font-bold text-black-700">8000원</p>
           </div>
         </div>
       </div>
     </div>
   )
-}
-
-const getTicketTypeKorean = (type: TicketType): string => {
-  switch (type) {
-    case "oneday":
-      return "당일"
-    case "period":
-      return "기간"
-    case "discount":
-      return "할인"
-    default:
-      return "시간"
-  }
 }
 
 const getBookableKorean = (type: Bookable): string => {
