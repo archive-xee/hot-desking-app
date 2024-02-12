f7d947cc7dfa673a23b1f2654302a7edfc87fbff
Wontae Choi <>
2024-02-12 11:49:41 +0900

    [Add] TicketInUse 추가 (카드형식)

---
+++ src/components/molecules/Ticket/TicketInUse.tsx
@@ -0,0 +1,85 @@
+import Image from "next/image";
+import type { Ticket, TicketType } from "@/src/models/ticket";
+import { Bookable } from "@/src/models/ticket";
+const TicketInUse = (props: { ticket: Ticket }) => {
+  const { ticket } = props;
+  const bookableKorean = getBookableKorean(ticket.bookable);
+  const ticketTypeKorean = getTicketTypeKorean(ticket.type);
+
+  const borderColors: { [key in Bookable]: string } = {
+    seat: "border-blue-100",
+    meetingroom: "border-yellow-500",
+    rentbox: "border-teal-100",
+    locker: "border-purple-100",
+  };
+
+  const bookableColors: { [key in Bookable]: string } = {
+    seat: "bg-blue-500",
+    meetingroom: "bg-yellow-500",
+    rentbox: "bg-teal-300",
+    locker: "bg-purple-500",
+  };
+
+  return (
+    <div className="flex flex-row justify-center">
+      <div
+        className={`flex ${borderColors[ticket.bookable]} w-56 flex-col overflow-hidden rounded-lg border border-solid bg-white-300`}
+      >
+        <div
+          className={`flex ${bookableColors[ticket.bookable]} flex-row items-center justify-center gap-2 py-2`}
+        >
+          <h3 className="text-lg font-bold text-white-100">
+            8{ticketTypeKorean}권
+          </h3>
+          <Image
+            src={`/icons/ticket/${ticket.type}.png`}
+            alt="이것도 해야겠네"
+            width="24"
+            height="24"
+          ></Image>
+        </div>
+        <div
+          className={`flex h-44 w-full flex-col items-center justify-center gap-1 py-4 `}
+        >
+          <Image
+            src={`/icons/bookable/${ticket.bookable}.png`}
+            alt="이것도 해야겠네"
+            width="48"
+            height="48"
+          ></Image>
+          <p className="font-bold text-black-700">{bookableKorean}</p>
+          <div className="grow"></div>
+          <p className="font-bold">유효기간</p>
+          <p>2024-01-12 ~ 2024-04-11</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const getBookableKorean = (type: Bookable): string => {
+  switch (type) {
+    case "seat":
+      return "좌석";
+    case "meetingroom":
+      return "미팅룸";
+    case "rentbox":
+      return "대여함";
+    default:
+      return "사물함";
+  }
+};
+
+const getTicketTypeKorean = (type: TicketType): string => {
+  switch (type) {
+    case "oneday":
+      return "당일";
+    case "period":
+      return "기간";
+    case "discount":
+      return "할인";
+    default:
+      return "시간";
+  }
+};
+export default TicketInUse;
