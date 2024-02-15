8f139bb642bb3c040b59d0188affc95013e23d6d
Wontae Choi <>
2024-02-15 16:06:24 +0900

    [Add] Bookable Mock 클래스 생성

--- src/models/bookable.ts
+++ src/models/bookable.ts
@@ -97,3 +97,53 @@ export class RentBoxModel extends Bookable {
     this.name = name;
   }
 }
+
+// MockClass
+export const history1 = new History(
+  "1",
+  "seat",
+  "1",
+  "1",
+  1707348182,
+  1707948182,
+);
+export const seat1 = new SeatModel("1", "wontae", "1", [history1], 1707148182);
+export const seat2 = new SeatModel("2", null, "2", [history1], 1707148182);
+export const meetingRoom1 = new MeetingRoomModel(
+  "1",
+  "wontae",
+  "1",
+  [history1],
+  1707148182,
+);
+export const meetingRoom2 = new MeetingRoomModel(
+  "2",
+  null,
+  "2",
+  [history1],
+  1707148182,
+);
+export const locker1 = new LockerModel(
+  "1",
+  "wontae",
+  "1",
+  [history1],
+  1707148182,
+);
+export const locker2 = new LockerModel("2", null, "2", [history1], 1707148182);
+export const rentbox1 = new RentBoxModel(
+  "1",
+  "wontae",
+  "1",
+  [history1],
+  1707148182,
+  "g102IC",
+);
+export const rentbox2 = new RentBoxModel(
+  "2",
+  null,
+  "2",
+  [history1],
+  1707148182,
+  "g102IC",
+);
