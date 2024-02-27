a23851cc996b61b0d6677f881be4cf5a6c0f6826
Wontae Choi <>
2024-02-27 18:26:54 +0900

    [Add] Seat, MeetingRoom, RentItem, Locker QR로 접속했을 때의 현황페이지에서 필요한 객체의 field

---
+++ src/app/booth/meetingroom/page.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+
+export default function MeetingRoomViewPage() {
+  return (
+    <>
+      <p>
+        QR코드에서 id를 gql에 담아서 요청을 보내면 응답은 MeetingRoom 클래스의 아래 필드만
+        <br /> ticketId: string | null
+      </p>
+
+      <Link href="/user/ticket/seat">
+        <button
+          type="submit"
+          className="rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
+        >
+          티켓 사용
+        </button>
+      </Link>
+    </>
+  )
+}
