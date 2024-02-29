be10e92fe36f21bd9a2bb84ace044bda68276ce3
Wontae Choi <>
2024-02-29 10:33:01 +0900

    [Add] 티켓 구매를 위해 모달 열였을 때 넘기는 Ticket + 결제버튼 누른 이후의 flow (결제객체 서버에 넘김)의 설명

--- src/app/ticket/page.tsx
+++ src/app/ticket/page.tsx
@@ -88,6 +88,13 @@ export default function TicketPage() {
           </Link>
         }
       />
+      <p>
+        <br /> Ticket[ id, ticketType, expiresAt ]의 리스트
+        <br /> 구매하기 위해 티켓을 눌렀을 때 나오는 모달에는
+        <br /> 선택된 티켓을 프로퍼티로 보내겠음
+        <br /> 일회성결제, 예약결제, 정기결제 3가지 옵션 보여줄 거임
+        <br /> 각 버튼을 누르면 각 결제타입에 맞는 Order객체를 서버에 보내겠음
+      </p>
     </>
   )
 }
