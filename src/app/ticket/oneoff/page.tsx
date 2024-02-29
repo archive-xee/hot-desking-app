bd2e03f59aa05d111c9129e72fdc43841c81e8d9
Wontae Choi <>
2024-02-29 15:23:46 +0900

    [Update] ListViewContainer가 쓰이는 모든 곳에서 더 이상 사용하지 않으므로 삭제

--- src/app/ticket/oneoff/page.tsx
+++ src/app/ticket/oneoff/page.tsx
@@ -1,91 +1,49 @@
 import Image from "next/image"
 import Link from "next/link"
 import { StretchTicket } from "@/src/components/molecules/Ticket"
-import ListViewContainer from "@/src/components/organisms/ListViewContainer"
-import { Modal } from "@/src/components/organisms/Modal"
 export default function TicketPage() {
   return (
     <>
-      <TicketPageTitle />
-      <ListViewContainer
-        title="정해진 기간동안 사용할 수 있습니다."
-        tabs={TicketTypeTab()}
-        listItems={
-          <>
-            <Modal
-              trigger={
-                <StretchTicket
-                  ticket={{
-                    id: "1",
-                    type: "oneday",
-                    bookable: "locker",
-                    price: 10000,
-                    period: 86400,
-                    issuedAt: 1706946429,
-                    expiresAt: 1707551229,
-                  }}
-                ></StretchTicket>
-              }
-              title="티켓입니다"
-              content={<></>}
-              actions={
-                <>
-                  <button
-                    type="button"
-                    className="rounded-lg border border-blue-700 bg-white-100 px-5 py-2 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
-                  >
-                    일회성결제
-                  </button>
-                  <button
-                    type="button"
-                    className="rounded-lg border border-blue-700 bg-white-100 px-5 py-2 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
-                  >
-                    정기결제
-                  </button>
-                  <button
-                    type="button"
-                    className="rounded-lg border border-blue-700 bg-white-100 px-5 py-2 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
-                  >
-                    예약결제
-                  </button>
-                </>
-              }
-            ></Modal>
-
-            <StretchTicket
-              ticket={{
-                id: "1",
-                type: "time",
-                bookable: "seat",
-                price: 10000,
-                period: 86400,
-                issuedAt: 1706946429,
-                expiresAt: 1707551229,
-              }}
-            ></StretchTicket>
-          </>
-        }
-        actions={
-          <>
-            <Link href="/payment/online">
-              <button
-                type="button"
-                className="rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
-              >
-                구매하기
-              </button>
-            </Link>
-            <Link href="/user/coupon">
-              <button
-                type="button"
-                className="rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
-              >
-                쿠폰 적용
-              </button>
-            </Link>
-          </>
-        }
-      />
+      <OneoffTicketPageTitle />
+      <OneoffTicketTypeTab />
+      <StretchTicket
+        ticket={{
+          id: "1",
+          type: "oneday",
+          bookable: "locker",
+          price: 10000,
+          period: 86400,
+          issuedAt: 1706946429,
+          expiresAt: 1707551229,
+        }}
+      ></StretchTicket>
+      <StretchTicket
+        ticket={{
+          id: "1",
+          type: "oneday",
+          bookable: "locker",
+          price: 10000,
+          period: 86400,
+          issuedAt: 1706946429,
+          expiresAt: 1707551229,
+        }}
+      ></StretchTicket>
+      <Link href="/payment/online">
+        <button
+          type="button"
+          className="rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
+        >
+          구매하기
+        </button>
+      </Link>
+      <Link href="/user/coupon">
+        <button
+          type="button"
+          className="rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
+        >
+          쿠폰 적용
+        </button>
+      </Link>
       <p>
         <br /> Ticket[ id, ticketType, expiresAt ]의 리스트
         <br /> 구매하기 위해 티켓을 눌렀을 때 나오는 모달에는
@@ -98,11 +56,11 @@ export default function TicketPage() {
   )
 }
 
-const TicketPageTitle = () => {
+const OneoffTicketPageTitle = () => {
   return <h1 className="text-center text-xl font-bold">이용권 구매하기</h1>
 }
 
-const TicketTypeTab = () => {
+const OneoffTicketTypeTab = () => {
   return (
     <div className="flex flex-row bg-white-300">
       <div className="flex grow flex-row  justify-center p-2  hover:bg-yellow-300">
--- src/app/user/ticket/[bookableType]/page.tsx
+++ src/app/user/ticket/[bookableType]/page.tsx
@@ -1,112 +1,58 @@
 import Link from "next/link"
-import { TicketOfUser } from "@/src/components/molecules/Ticket"
-import ListViewContainer from "@/src/components/organisms/ListViewContainer"
+import { StretchTicket } from "@/src/components/molecules/Ticket"
 
 export default function UserSeatTicketPage() {
   return (
     <>
       <UserTicketPageTitle />
-      <ListViewContainer
-        title="나의 좌석 이용권"
-        listItems={
-          <>
-            <TicketOfUser
-              ticket={{
-                id: "1",
-                type: "time",
-                bookable: "seat",
-                price: 10000,
-                period: 86400,
-                issuedAt: 1706946429,
-                expiresAt: 1707551229,
-              }}
-            ></TicketOfUser>
-            <TicketOfUser
-              ticket={{
-                id: "1",
-                type: "time",
-                bookable: "seat",
-                price: 10000,
-                period: 86400,
-                issuedAt: 1706946429,
-                expiresAt: 1707551229,
-              }}
-            ></TicketOfUser>
-            <TicketOfUser
-              ticket={{
-                id: "1",
-                type: "time",
-                bookable: "seat",
-                price: 10000,
-                period: 86400,
-                issuedAt: 1706946429,
-                expiresAt: 1707551229,
-              }}
-            ></TicketOfUser>
-            <TicketOfUser
-              ticket={{
-                id: "1",
-                type: "time",
-                bookable: "seat",
-                price: 10000,
-                period: 86400,
-                issuedAt: 1706946429,
-                expiresAt: 1707551229,
-              }}
-            ></TicketOfUser>
-            <TicketOfUser
-              ticket={{
-                id: "1",
-                type: "time",
-                bookable: "seat",
-                price: 10000,
-                period: 86400,
-                issuedAt: 1706946429,
-                expiresAt: 1707551229,
-              }}
-            ></TicketOfUser>
-            <TicketOfUser
-              ticket={{
-                id: "1",
-                type: "time",
-                bookable: "seat",
-                price: 10000,
-                period: 86400,
-                issuedAt: 1706946429,
-                expiresAt: 1707551229,
-              }}
-            ></TicketOfUser>
-          </>
-        }
-        actions={
-          <div className="flex flex-row gap-2">
-            <Link href="/ticket/used">
-              <button
-                type="button"
-                className="rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
-              >
-                사용 완료
-              </button>
-            </Link>
-            <Link href="/ticket/oneoff">
-              <button
-                type="button"
-                className="rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
-              >
-                일회권 구매
-              </button>
-            </Link>
-            <Link href="/ticket/billing">
-              <button
-                type="button"
-                className="rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
-              >
-                정기권 구매
-              </button>
-            </Link>
-          </div>
-        }
-      ></ListViewContainer>
+      <StretchTicket
+        ticket={{
+          id: "1",
+          type: "time",
+          bookable: "seat",
+          price: 10000,
+          period: 86400,
+          issuedAt: 1706946429,
+          expiresAt: 1707551229,
+        }}
+      ></StretchTicket>
+      <StretchTicket
+        ticket={{
+          id: "1",
+          type: "time",
+          bookable: "seat",
+          price: 10000,
+          period: 86400,
+          issuedAt: 1706946429,
+          expiresAt: 1707551229,
+        }}
+      ></StretchTicket>
+      <div className="flex flex-row gap-2">
+        <Link href="/ticket/used">
+          <button
+            type="button"
+            className="rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
+          >
+            사용 완료
+          </button>
+        </Link>
+        <Link href="/ticket/oneoff">
+          <button
+            type="button"
+            className="rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
+          >
+            일회권 구매
+          </button>
+        </Link>
+        <Link href="/ticket/billing">
+          <button
+            type="button"
+            className="rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
+          >
+            정기권 구매
+          </button>
+        </Link>
+      </div>
       <p>
         url route의 맞는 bookableType에 따라 UserTicket [ id, ticketType, expiresAt, userId ]의 리스트를 요청
         <br />
