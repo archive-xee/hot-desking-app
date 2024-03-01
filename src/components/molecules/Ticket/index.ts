83478fff726cb3aaebbd65298bb3611a7ced2e7f
Wontae Choi <>
2024-03-01 14:56:58 +0900

    [Remove] 사용되지 않는 TicketInUse 컴포넌트/파일 삭제

--- src/components/molecules/Ticket/index.ts
+++ src/components/molecules/Ticket/index.ts
@@ -1,6 +1,5 @@
 import CardTicket from "@/src/components/molecules/Ticket/CardTicket"
 import StretchTicket from "@/src/components/molecules/Ticket/StretchTicket"
-import TicketInUse from "@/src/components/molecules/Ticket/TicketInUse"
 import TicketOfUser from "@/src/components/molecules/Ticket/TicketOfUser"
 
-export { StretchTicket, TicketOfUser, CardTicket, TicketInUse }
+export { StretchTicket, TicketOfUser, CardTicket }
