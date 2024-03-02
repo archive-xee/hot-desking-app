8c32eb7211c9fbdc8fe9ac971fe23632cadf8987
Wontae Choi <>
2024-03-02 16:30:50 +0900

    [Tmp] /booth (QR 랜딩페이지) tmp gql 완성

--- src/app/booth/locker/page.tsx
+++ src/app/booth/locker/page.tsx
@@ -1,17 +1,40 @@
+"use client"
+
+import { gql } from "@apollo/client"
+import { useQuery } from "@apollo/experimental-nextjs-app-support/ssr"
 import Link from "next/link"
+import { useSearchParams } from "next/navigation"
+
+const GET_TICKET_ID = gql`
+  query GetTicketId($lockerId: String!) {
+    Locker(id: $lockerId) {
+      ticketId
+    }
+  }
+`
 
 export default function LockerViewPage() {
+  const searchParams = useSearchParams()
+  // 궁창/booth/locker/?id=1
+  // const lockerId = searchParams.get("id")
+  // const { data } = useQuery(GET_TICKET_ID, {
+  //   variables: { lockerId },
+  // })
+
+  // if (loading) return "Loading..."
+  // if (error) return `Error! ${error.message}`
+
   return (
     <>
       <p>
         QR코드에서 id를 gql에 담아서 요청을 보내면 응답은 Seat 클래스의 아래 필드만
         <br /> ticketId: string | null
       </p>
-      <div className="border">gql</div>
+      <div className="border">query GetTicketId($lockerId: String!) [ locker(id: $lockerId) [ ticketId ] ]</div>
       <Link href="/user/ticket/locker">
         <button
           type="submit"
-          className="rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
+          className="bg-white-100 hover:text-white-100 rounded-lg border border-blue-700 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 "
         >
           티켓 사용
         </button>
--- src/app/booth/rentitem/page.tsx
+++ src/app/booth/rentitem/page.tsx
@@ -1,6 +1,28 @@
-import Link from "next/link"
+"use client"
 
+import { gql } from "@apollo/client"
+import { useQuery } from "@apollo/experimental-nextjs-app-support/ssr"
+import Link from "next/link"
+import { useSearchParams } from "next/navigation"
+
+const GET_TICKET_ID = gql`
+  query GetTicketId($rentItemId: String!) {
+    RentItem(id: $rentItemId) {
+      ticketId
+      itemName
+    }
+  }
+`
 export default function RentItemViewPage() {
+  const searchParams = useSearchParams()
+  // 궁창/booth/rentItem/?id=1
+  const rentItemId = searchParams.get("id")
+  // const { data } = useQuery(GET_TICKET_ID, {
+  //   variables: { rentItemId },
+  // })
+
+  // if (loading) return "Loading..."
+  // if (error) return `Error! ${error.message}`
   return (
     <>
       <p>
@@ -8,11 +30,13 @@ export default function RentItemViewPage() {
         <br /> ticketId: string | null
         <br /> itemName: string
       </p>
-      <div className="border">gql</div>
+      <div className="border">
+        query GetTicketId($rentItemId: String!) [ RentItem(id: $rentItemId) [ ticketId itemName ] ]
+      </div>
       <Link href="/user/ticket/rentitem">
         <button
           type="submit"
-          className="rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
+          className="bg-white-100 hover:text-white-100 rounded-lg border border-blue-700 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 "
         >
           티켓 사용
         </button>
