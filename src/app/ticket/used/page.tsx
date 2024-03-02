7606b736d5826096aa277ec19e4ae3250ca44e16
Wontae Choi <>
2024-03-02 22:20:18 +0900

    [Tmp] 티켓/쿠폰 사용후 보이는 페이지 → 이후 다이얼로그로 대체 예정

--- src/app/ticket/used/page.tsx
+++ src/app/ticket/used/page.tsx
@@ -2,7 +2,9 @@ export default function TicketUsedPage() {
   return (
     <>
       사용 완료
-      <div className="border">gql</div>
+      <p>
+        사용된 이용권은 파라미터로 넘길 것이기 때문에 여기서는 gql을 받아오는게 없음
+      </p>
     </>
   )
 }
