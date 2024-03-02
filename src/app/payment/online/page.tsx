c9359c3abd7fa71279534f8e15ab7fecc02ea939
Wontae Choi <>
2024-03-02 23:11:08 +0900

    [Tmp] 온라인결제, 카드등록 tmp gql 완성

--- src/app/payment/online/page.tsx
+++ src/app/payment/online/page.tsx
@@ -1,16 +1,30 @@
+import { gql } from "@apollo/client"
 import Link from "next/link"
+
+const GET_USER_CARD_LIST = gql`
+  query GetUserCardList($userId: string) {
+    User(userId: $userId) {
+      card
+    } 
+  }
+`
+
 export default function PaymentOnlinePage() {
   return (
     <>
       일회권 온라인 결제
+      <p>유저 카드목록</p>
       {/* 정기권 온라인결제랑 구별해야함 */}
-      <div className="border">gql</div>
+      <div className="border"> query GetUserCardList($userId: string) [
+    User(userId: $userId) [
+      card
+    ] 
+      ]</div>
       <Link href="/card/register">
         <button>카드 등록하기</button>
       </Link>
-      {/* portone 결제 요청 이후 리디렉션 */}
       <Link href="/user/ticket/oneoff">
-        <button>동의하고 결제하기</button>
+        <button>동의하고 결제하기(portone 결제 요청 이후 리디렉션)</button>
       </Link>
     </>
   )
