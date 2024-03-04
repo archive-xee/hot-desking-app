062cdfcd1d7ffaa5d931dbcba24d076cf8cf7059
Wontae Choi <>
2024-03-04 17:34:29 +0900

    [Add] 유저쿠폰페이지에  CouponRegistrationButton(쿠폰등록버튼) 추가

--- src/app/user/coupon/page.tsx
+++ src/app/user/coupon/page.tsx
@@ -17,6 +17,7 @@ export default function UserCouponPage() {
   return (
     <>
       <UserCouponPageTitle />
+      <CouponRegistrationButton/>
       <BottomSheetModal trigger={<StretchedCoupon
         coupon={{
           id: "1",
@@ -41,16 +42,6 @@ export default function UserCouponPage() {
       ></StretchedCoupon>} buttonTitle="사용하기" buttonAction={() => {
         router.back()
       } }      ></BottomSheetModal>
-      <div className="flex flex-row gap-2">
-        <Link href="/coupon">
-          <button
-            type="button"
-            className="bg-white-100 hover:text-white-100 rounded-lg border border-blue-700 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 "
-          >
-            등록하기
-          </button>
-        </Link>
-      </div>
       <p>
         <br /> UserTimeBonusCoupon[ id, userId, bonusTime, CouponType, expiresAt ]의 리스트 + UserDiscountCoupon [ id,
         CouponType , discountPercentage, expiresAt]의 리스트
@@ -64,4 +55,16 @@ export default function UserCouponPage() {
 
 const UserCouponPageTitle = () => {
   return <h1 className="text-center text-xl font-bold">내 쿠폰 리스트</h1>
+}
+
+
+const CouponRegistrationButton = () => {
+  return   <Link href="/coupon">
+  <button
+    type="button"
+    className="w-full bg-white-100 hover:text-white-100 rounded-lg border border-blue-700 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 "
+  >
+    쿠폰 등록하기
+  </button>
+</Link>
 }
