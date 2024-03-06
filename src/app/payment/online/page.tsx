3a847e74d8f9822ae4f014661edbae3b829abc3f
Wontae Choi <>
2024-03-06 17:12:44 +0900

    [Add] 나이스페이먼츠 v1 Script 추가

--- src/app/payment/online/page.tsx
+++ src/app/payment/online/page.tsx
@@ -1,5 +1,10 @@
+"use client"
+
 // import { gql } from "@apollo/client"
 import Link from "next/link"
+import Script from "next/script"
+// import { useSearchParams } from "next/navigation"
+// import { useState, useEffect } from "react"
 
 // const GET_USER_CARD_LIST = gql`
 //   query GetUserCardList($userId: string) {
@@ -8,20 +13,76 @@ import Link from "next/link"
 //     }
 //   }
 // `
-
 export default function PaymentOnlinePage() {
+  // 티켓 아이디 or 티켓name만 넘기면 사실은 됨
+  // const searchParams = useSearchParams()
+  // const ticketId = searchParams.get("ticketId")
+  let hasConsent = false // eslint-disable-line
+  // const [response, setResponse] = useState<PaymentResponse | undefined>(undefined)
+
+  // 티켓정보를 받을 것
+  // 정기권인지, 일회권인지에 따라 예약결제가 동의약관이 보일 것
+  // 유저가 예약결제에 동의했는지도 db에 들어가야 함
   return (
     <>
-      일회권 온라인 결제
-      <p>유저 카드목록</p>
-      {/* 정기권 온라인결제랑 구별해야함 */}
+      <div className="flex flex-col items-center">
+        일회권 온라인 결제
+        <UserCardList />
+        <div className="flex items-center">
+          <input
+            id="billing-consent-checkbox"
+            type="checkbox"
+            onChange={({ target: { checked } }) => {
+              hasConsent = !checked
+            }}
+            className="bg-white-100 text-black-700 size-4 rounded"
+          />
+          <label
+            htmlFor="billing-consent-checkbox"
+            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            예약 결제
+          </label>
+        </div>
+        <div>
+          <Link href="/card/register">
+            <button
+              type="button"
+              className=" bg-white-100 hover:text-white-100 inline-block rounded-lg border border-blue-700 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 "
+            >
+              카드등록
+            </button>
+          </Link>
+          <button
+            type="button"
+            onClick={() => {}}
+            className=" bg-white-100 hover:text-white-100 inline-block rounded-lg border border-blue-700 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 "
+          >
+            결제하기
+          </button>
+        </div>
+      </div>
+      portone 결제 요청 이후 /user/ticket/oneoff으로 리디렉션
       <div className="border"> query GetUserCardList($userId: string) [ User(userId: $userId) [ card ] ]</div>
-      <Link href="/card/register">
-        <button>카드 등록하기</button>
-      </Link>
-      <Link href="/user/ticket/oneoff">
-        <button>동의하고 결제하기(portone 결제 요청 이후 리디렉션)</button>
-      </Link>
+      <Script src="https://pay.nicepay.co.kr/v1/js/" />
     </>
+  )
+}
+
+const UserCardList = () => {
+  return (
+    <div className="flex flex-col">
+      <div>카드일번</div>
+      <div>카드일번</div>
+      <div>카드일번</div>
+      {/*
+  필요한 것
+
+  카드번호
+  유효기간(년/월) 
+  생년월일(6자리) 
+  비밀번호 앞 2자리 
+ */}
+    </div>
   )
 }
