1171034db53a978d698fe1f42383a322df496c4a
Wontae Choi <>
2024-03-12 11:38:55 +0900

    [Add] 서버의 리디렉션 페이지(/payment/online/response)

---
+++ src/app/payment/online/response/page.tsx
@@ -0,0 +1,8 @@
+export default function PaymentOnlineResponsePage() {
+  return (
+    <>
+      온라인 결제 결과 리디렉션의 파라미터에 따라 보여줄 것 setTimeOut등으로 티켓구매페이지로 돌아가게끔 설정할 것
+      <div className="border">gql</div>
+    </>
+  )
+}
