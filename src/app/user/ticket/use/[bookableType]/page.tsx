a0723e17653065cd8a27405b7dd9d0058710d09b
Wontae Choi <>
2024-03-14 17:18:45 +0900

    [Add] 티켓구매페이지에 BottomSheetModal 추가 + gql 업데이트

--- src/app/user/ticket/use/[bookableType]/page.tsx
+++ src/app/user/ticket/use/[bookableType]/page.tsx
@@ -3,6 +3,7 @@
 // import { gql } from "@apollo/client"
 import Link from "next/link"
 import StretchedTicket from "@/components/molecules/Ticket/StretchedTicket"
+import BottomSheetModal from "@/components/organisms/BottomSheetModal"
 
 // const GET_USER_COUPON_LIST = gql`
 //   query GetUserTicketList($userId: string) {
@@ -13,41 +14,33 @@ import StretchedTicket from "@/components/molecules/Ticket/StretchedTicket"
 //     }
 //   }
 // `
+
 export default function UserSeatTicketPage() {
   return (
     <>
       <UserTicketPageTitle />
-      <StretchedTicket
-        ticket={{
-          id: "1",
-          type: "time",
-          bookable: "seat",
-          price: 10000,
-          period: 86400,
-          issuedAt: 1706946429,
-          expiresAt: 1707551229,
-        }}
-      ></StretchedTicket>
-      <StretchedTicket
-        ticket={{
-          id: "1",
-          type: "time",
-          bookable: "seat",
-          price: 10000,
-          period: 86400,
-          issuedAt: 1706946429,
-          expiresAt: 1707551229,
-        }}
-      ></StretchedTicket>
+      <BottomSheetModal
+        trigger={
+          <StretchedTicket
+            ticket={{
+              id: "1",
+              type: "time",
+              bookable: "seat",
+              price: 10000,
+              period: 86400,
+              issuedAt: 1706946429,
+              expiresAt: 1707551229,
+            }}
+          ></StretchedTicket>
+        }
+        content={
+          <div className="flex flex-row justify-center">
+            <button className="rounded-full bg-white-100 px-10 py-2 font-bold text-black-700">사용하기</button>
+          </div>
+        }
+      ></BottomSheetModal>
+
       <div className="flex flex-row gap-2">
-        <Link href="/ticket/used">
-          <button
-            type="button"
-            className="rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
-          >
-            사용 완료
-          </button>
-        </Link>
         <Link href="/ticket/oneoff">
           <button
             type="button"
@@ -65,12 +58,17 @@ export default function UserSeatTicketPage() {
           </button>
         </Link>
       </div>
-      <p>
-        url route의 맞는 bookableType에 따라 UserTicket [ id ticketType expiresAt, userId ]의 리스트를 요청
-        <br />
-        사용하기 위해 티켓을 눌렀을 때 나오는 모달에는 선택된 티켓을 프로퍼티로 보내겠음
-      </p>
-      <div className="border">query GetUserTicketList($userId: string) [ UserTicket(userId: $userId) ]</div>
+      <div className="border">
+        <p>url route의 맞는 bookableType에 따라 UserTicket [ id ticketType expiresAt, userId ]의 리스트를 요청</p>
+        <p>query GetUserTicketList($userId: string) [ UserTicket(userId: $userId) ]</p>
+      </div>
+      <div className="border">
+        <p>사용하기 위해 티켓을 눌렀을 때 나오는 모달에는 선택된 티켓을 프로퍼티로 보냄</p>
+        <p>
+          mutation ActivateUserTicket($userId: string, $userTicketId: string) [ UserTicket(id: $userTicketId, activated:
+          true) ]
+        </p>
+      </div>
     </>
   )
 }
