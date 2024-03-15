e7c7ad45bc1c2b749448fcd6b86a3c436b63e5e3
Wontae Choi <>
2024-03-15 13:06:25 +0900

    [Add] 카카오로그인 Full SDK 2.7.0 버전 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script"
+
 export default function LoginPage() {
   return (
     <>
@@ -54,6 +56,11 @@ const LoginForm = () => {
           </div>
         </form>
       </div>
+      <Script
+        src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.0/kakao.min.js"
+        integrity="sha384-l+xbElFSnPZ2rOaPrU//2FF5B4LB8FiX5q4fXYTlfcG4PGpMkE1vcL7kNXI6Cci0"
+        crossOrigin="anonymous"
+      ></Script>
     </div>
   )
 }
