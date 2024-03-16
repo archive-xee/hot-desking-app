d29af09e4e7e2e7c280661c86e65a02ad779db76
Wontae Choi <>
2024-03-16 16:14:59 +0900

    [Update] 카카오로그인 상수 업데이트

--- src/constant/kakaoauth.ts
+++ src/constant/kakaoauth.ts
@@ -1,2 +1,5 @@
-export const KAKAO_AUTH_REDIRECT_URL = "https://dev.ultspace.com/oauth"
 export const KAKAO_AUTH_JAVASCRIPT_KEY = "c20aecc79e092c5f60c418d60add6730"
+export const KAKAO_AUTH_RESTAPI_KEY = "32f7e76cd0e60d900c160788b7efa9dd"
+export const KAKAO_AUTH_CLIENT_SECRET_KEY = "We9r7vFDQ89nKQwYxZld9fx6lh2V5nuU"
+export const KAKAO_AUTH_CODE_REDIRECT_URL = "http://127.0.0.1:3000/api/oauth/kakao"
+export const KAKAO_TOKEN_REQUEST_URL = "https://kauth.kakao.com/oauth/token"
