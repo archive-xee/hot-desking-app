b9fad70e6438cb20d08895d6b10a3e87dfac5f65
Wontae Choi <>
2024-03-18 16:41:33 +0900

    [Add] authOptions에 session, jwt, callbacks 추가

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,62 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
 import KakaoProvider from "next-auth/providers/kakao"
-import { KAKAO_AUTH_JAVASCRIPT_KEY, NEXTAUTH_KAKAO_CLIENT_SECRET } from "@/constant/kakaoauth"
+import { KAKAO_AUTH_JAVASCRIPT_KEY, NEXTAUTH_KAKAO_CLIENT_SECRET, NEXTAUTH_SECRET } from "@/constant/kakaoauth"
 
-const authOptions = {
+declare module "next-auth" {
+  /**
+   * The shape of the user object returned in the OAuth providers' `profile` callback,
+   * or the second parameter of the `session` callback, when using a database.
+   */
+  interface User {
+    name: string
+  }
+  /**
+   * The shape of the account object returned in the OAuth providers' `account` callback,
+   * Usually contains information about the provider being used, like OAuth tokens (`access_token`, etc).
+   */
+  //   interface Account {}
+
+  /**
+   * Returned by `useSession`, `auth`, contains information about the active session.
+   */
+  interface Session {
+    accessToken: string
+  }
+}
+
+// declare module "next-auth/jwt" {
+//   /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+//   interface JWT {
+//     /** OpenID ID Token */
+//     idToken?: string
+//   }
+// }
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: NEXTAUTH_SECRET,
+    // secret: "process.env.NEXTAUTH_SECRET",
+  },
   providers: [
     KakaoProvider({
       clientId: KAKAO_AUTH_JAVASCRIPT_KEY,
       clientSecret: NEXTAUTH_KAKAO_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user }
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string
+      return session
+    },
+  },
 }
+
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
--- src/constant/kakaoauth.ts
+++ src/constant/kakaoauth.ts
@@ -1,2 +1,3 @@
 export const KAKAO_AUTH_JAVASCRIPT_KEY = "c20aecc79e092c5f60c418d60add6730"
 export const NEXTAUTH_KAKAO_CLIENT_SECRET = "We9r7vFDQ89nKQwYxZld9fx6lh2V5nuU"
+export const NEXTAUTH_SECRET = "We9r7vFDQ89nKQwYxZld9fx6lh2V5nuU"
