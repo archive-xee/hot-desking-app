71b8a748bf832ec2ba66a505d5674b0fa68ca7f9
Wontae Choi <>
2024-03-19 15:07:07 +0900

    [Add] payment/online에서 status === authenticated일 때 userId 받아옴

--- src/app/payment/online/oneoff/page.tsx
+++ src/app/payment/online/oneoff/page.tsx
@@ -2,7 +2,9 @@
 
 // import { gql } from "@apollo/client"
 import Link from "next/link"
+import { redirect } from "next/navigation"
 import Script from "next/script"
+import { useSession } from "next-auth/react"
 import { v4 as uuidv4 } from "uuid"
 import CardWithDeleteForm from "@/components/molecules/CardWithDeleteForm"
 import { executeAuthPaymentPopup } from "@/lib/nicepay"
@@ -19,7 +21,7 @@ import { card1 } from "@/models/card"
 // `
 
 export default function PaymentOnlinePage() {
-  // 티켓 아이디 or 티켓name만 넘기면 사실은 됨
+  const { data: session, status: authStatus } = useSession()
   // const searchParams = useSearchParams()
   // const ticketId = searchParams.get("ticketId")
   let hasConsent = false // eslint-disable-line
@@ -28,69 +30,74 @@ export default function PaymentOnlinePage() {
   // 티켓정보를 받을 것
   // 정기권인지, 일회권인지에 따라 예약결제가 동의약관이 보일 것
   // 유저가 예약결제에 동의했는지도 db에 들어가야 함
-  return (
-    <>
-      <div className="flex flex-col items-center">
-        일회권 온라인 결제
-        <div>
-          <input
-            id="payment-registered-card"
-            type="checkbox"
-            // onChange={({ target: { checked } }) => {}}
-            className="size-4 rounded bg-white-100 text-black-700"
-          />
-          <label
-            htmlFor="payment-registered-card"
-            className="text-gray-900 dark:text-gray-300 ms-2 text-sm font-medium"
-          >
-            등록된 카드로 결제
-          </label>
-        </div>
-        <UserCardList />
-        <div>
-          <Link href="/card/register">
-            <button
-              type="button"
-              className=" inline-block rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
+  if (authStatus === "loading") return <>로딩중</>
+  else if (authStatus === "unauthenticated") {
+    redirect("/auth/signin")
+  } else
+    return (
+      <>
+        <div className="flex flex-col items-center">
+          일회권 온라인 결제
+          {session?.userId}
+          <div>
+            <input
+              id="payment-registered-card"
+              type="checkbox"
+              // onChange={({ target: { checked } }) => {}}
+              className="size-4 rounded bg-white-100 text-black-700"
+            />
+            <label
+              htmlFor="payment-registered-card"
+              className="text-gray-900 dark:text-gray-300 ms-2 text-sm font-medium"
             >
-              카드등록
-            </button>
-          </Link>
-        </div>
-        <div>
-          <input
-            id="payment-registered-card"
-            type="checkbox"
-            // onChange={({ target: { checked } }) => {}}
-            className="size-4 rounded bg-white-100 text-black-700"
-          />
-          <label
-            htmlFor="payment-registered-card"
-            className="text-gray-900 dark:text-gray-300 ms-2 text-sm font-medium"
+              등록된 카드로 결제
+            </label>
+          </div>
+          <UserCardList />
+          <div>
+            <Link href="/card/register">
+              <button
+                type="button"
+                className=" inline-block rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
+              >
+                카드등록
+              </button>
+            </Link>
+          </div>
+          <div>
+            <input
+              id="payment-registered-card"
+              type="checkbox"
+              // onChange={({ target: { checked } }) => {}}
+              className="size-4 rounded bg-white-100 text-black-700"
+            />
+            <label
+              htmlFor="payment-registered-card"
+              className="text-gray-900 dark:text-gray-300 ms-2 text-sm font-medium"
+            >
+              앱카드 결제
+            </label>
+          </div>
+          <button
+            type="button"
+            onClick={() => {
+              executeAuthPaymentPopup({
+                orderId: uuidv4(),
+                ticketName: "온라인 일회권",
+                price: 1000,
+                paymentMethod: "cardAndEasyPay",
+              })
+            }}
+            className=" inline-block rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
           >
-            앱카드 결제
-          </label>
+            결제하기
+          </button>
         </div>
-        <button
-          type="button"
-          onClick={() => {
-            executeAuthPaymentPopup({
-              orderId: uuidv4(),
-              ticketName: "온라인 일회권",
-              price: 1000,
-              paymentMethod: "cardAndEasyPay",
-            })
-          }}
-          className=" inline-block rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
-        >
-          결제하기
-        </button>
-      </div>
-      portone 결제 요청 이후 /user/ticket/oneoff으로 리디렉션
-      <div className="border"> query GetUserCardList($userId: string) [ User(userId: $userId) [ card ] ]</div>
-      <Script src="https://pay.nicepay.co.kr/v1/js/" />
-    </>
-  )
+        portone 결제 요청 이후 /user/ticket/oneoff으로 리디렉션
+        <div className="border"> query GetUserCardList($userId: string) [ User(userId: $userId) [ card ] ]</div>
+        <Script src="https://pay.nicepay.co.kr/v1/js/" />
+      </>
+    )
 }
 
 const UserCardList = () => {
