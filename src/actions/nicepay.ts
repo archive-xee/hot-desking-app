ed852cbd0233bece1d821c39bbaecc01a566fea0
Wontae Choi <>
2024-03-20 19:43:20 +0900

    [Update] BillingKeyTerm 서버액션, BillingKeyTerm을 사용하는 PaymentOnlineBillingPage도 서버컴포넌트화

--- src/actions/nicepay.ts
+++ src/actions/nicepay.ts
@@ -4,6 +4,33 @@ import request, { gql } from "graphql-request"
 import { APOLLO_ROUTER_URL } from "@/constant/graphql"
 import { Card } from "@/models/card"
 
+export async function getBillingKeyTerm() {
+  const GET_BILLING_KEY_TERM_QUERY = gql`
+    query GetBillingKeyTerm($type: TermsType!) {
+      term(type: $type) {
+        termsTitle
+        content
+      }
+    }
+  `
+
+  type BillingKeyTermRequest = {
+    type: "eletorn" | "colletperson" | "sharinginfo" | "telcommun"
+  }
+
+  type BillingKeyTermResponse = {
+    termsTitle: string
+    content: string
+  }
+
+  const data: { term: BillingKeyTermResponse } = await request(APOLLO_ROUTER_URL, GET_BILLING_KEY_TERM_QUERY, {
+    type: "eletorn",
+  } as BillingKeyTermRequest)
+
+  const billingKeyTerm = data.term
+  return billingKeyTerm
+}
+
 export async function getUserCardList(userId: string) {
   const GET_USER_CARD_LIST = gql`
     query GetUserCardList($userId: String!) {
--- src/app/payment/online/billing/page.tsx
+++ src/app/payment/online/billing/page.tsx
@@ -1,14 +1,9 @@
-"use client"
-
 import Link from "next/link"
-import { redirect } from "next/navigation"
-import { useSession } from "next-auth/react"
+import BillingKeyTerm from "@/components/organisms/BillingKeyTerm"
 import UserCardList from "@/components/organisms/UserCardList"
 // import { useSearchParams } from "next/navigation"
 
-export default function PaymentOnlinePage() {
-  const { data: session, status: authStatus } = useSession()
-
+export default function PaymentOnlineBillingPage() {
   // 유저가 예약결제에 동의했는지도 db에 들어가야 함
   // const searchParams = useSearchParams()
   // const ticketId = searchParams.get("ticketId")
@@ -16,42 +11,38 @@ export default function PaymentOnlinePage() {
   // 티켓id를 path에서 받을 것
   // 정기권인지, 일회권인지에 따라 예약결제가 동의약관이 보일 것
   // 유저가 예약결제에 동의했는지도 db에 들어가야 함
-  if (authStatus === "loading") return <>로딩중</>
-  else if (authStatus === "unauthenticated") {
-    redirect("/auth/signin")
-  } else
-    return (
-      <>
-        <div className="flex flex-col items-center">
-          정기권 온라인 결제
-          <UserCardList userId={session?.userId!} />
-          <div>
-            <Link href="/card/register">
-              <button
-                type="button"
-                className=" inline-block rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
-              >
-                카드등록
-              </button>
-            </Link>
-          </div>
-          <div>약관내용 (cheveron)</div>
-          <div>
-            <input
-              id="payment-registered-card"
-              type="checkbox"
-              // onChange={({ target: { checked } }) => {}}
-              className="size-4 rounded bg-white-100 text-black-700"
-            />
-            <label
-              htmlFor="payment-registered-card"
-              className="text-gray-900 dark:text-gray-300 ms-2 text-sm font-medium"
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        정기권 온라인 결제
+        <UserCardList />
+        <div>
+          <Link href="/card/register">
+            <button
+              type="button"
+              className=" inline-block rounded-lg border border-blue-700 bg-white-100 px-5 py-2.5 text-sm font-medium hover:bg-blue-300 hover:text-white-100 "
             >
-              약관에 동의하고 진행하기
-            </label>
-          </div>
+              카드등록
+            </button>
+          </Link>
         </div>
-        결제 응답에 따라 성공하면 사용페이지로, 실패하면 실패 페이지로 리디렉션
-      </>
-    )
+        <BillingKeyTerm />
+        <div>
+          <input
+            id="payment-registered-card"
+            type="checkbox"
+            // onChange={({ target: { checked } }) => {}}
+            className="size-4 rounded bg-white-100 text-black-700"
+          />
+          <label
+            htmlFor="payment-registered-card"
+            className="text-gray-900 dark:text-gray-300 ms-2 text-sm font-medium"
+          >
+            약관에 동의하고 진행하기
+          </label>
+        </div>
+      </div>
+      결제 응답에 따라 성공하면 사용페이지로, 실패하면 실패 페이지로 리디렉션
+    </>
+  )
 }
