5b2edf27eb0bcfea88d760cbdaacacf97c3fdd8e
Wontae Choi <>
2024-03-31 16:23:47 +0900

    [Remove] input을 직접 validation하려 했던 시도는 버그가 많아 중단

--- src/components/organisms/Card/index.ts
+++
@@ -1,7 +0,0 @@
-import CardIdNoInput from "./CardIdNoInput"
-import CardMonthInput from "./CardMonthInput"
-import CardNumberInput from "./CardNumberInput"
-import CardPasswordInput from "./CardPasswordInput"
-import CardYearInput from "./CardYearInput"
-
-export { CardIdNoInput, CardMonthInput, CardPasswordInput, CardNumberInput, CardYearInput }
