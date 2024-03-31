f67e013a5e3a07efcac86cde8b42dbc65a0a7f74
Wontae Choi <>
2024-04-01 01:02:50 +0900

    [Update] 정기권결제페이지 완성(리디렉션 인스트럭션은 서버액션 완성 후 삭제)

--- src/app/(authheader)/payment/online/billing/page.tsx
+++ src/app/(authheader)/payment/online/billing/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import Button from "@/components/molecules/Button/Button"
+import Title from "@/components/molecules/Title"
 import BillingKeyTerm from "@/components/organisms/BillingKeyTerm"
 import UserCardList from "@/components/organisms/UserCardList"
 // import { useSearchParams } from "next/navigation"
@@ -13,32 +14,15 @@ export default function PaymentOnlineBillingPage() {
   // 정기권인지, 일회권인지에 따라 예약결제가 동의약관이 보일 것
   // 유저가 예약결제에 동의했는지도 db에 들어가야 함
   return (
-    <>
-      <div className="flex flex-col items-center">
-        정기권 온라인 결제
-        <UserCardList />
-        <div>
-          <Link href="/card/register">
-            <Button>카드등록</Button>
-          </Link>
-        </div>
-        <BillingKeyTerm />
-        <div>
-          <input
-            id="payment-registered-card"
-            type="checkbox"
-            // onChange={({ target: { checked } }) => {}}
-            className="size-4 rounded bg-white-100 text-black-700"
-          />
-          <label
-            htmlFor="payment-registered-card"
-            className="text-gray-900 dark:text-gray-300 ms-2 text-sm font-medium"
-          >
-            약관에 동의하고 진행하기
-          </label>
-        </div>
+    <div className="flex flex-col gap-2">
+      <Title text="정기권 온라인 결제" />
+      <UserCardList />
+      <div className="flex flex-row justify-end">
+        <Link href="/card/register">
+          <Button>카드등록</Button>
+        </Link>
       </div>
-      결제 응답에 따라 성공하면 사용페이지로, 실패하면 실패 페이지로 리디렉션
-    </>
+      결제이후 /user/ticket/use/(bookableType)으로 리디렉션 결제실패시에는 실패 페이지로
+    </div>
   )
 }
