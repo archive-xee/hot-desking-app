be56192e8dffd6485c3b86734c86a52fc6f020f5
Wontae Choi <>
2024-03-31 23:48:40 +0900

    [Add] UserCardList에서 card를 받냐 아니냐 분기에 따라 안내메시지 다르게 설정

--- src/components/organisms/UserCardList.tsx
+++ src/components/organisms/UserCardList.tsx
@@ -1,19 +1,28 @@
+import { Dialog } from "./Dialog"
 import { getUserIdAfterCheckAuthRedirect } from "@/actions/authjs"
 import { getUserCardList } from "@/actions/nicepay"
 import CardWithDeleteForm from "@/components/molecules/CardWithDeleteForm"
-import { Card } from "@/models/card"
+import { Card, card1, card2, card3 } from "@/models/card"
 
 export default async function UserCardList() {
   const userId = await getUserIdAfterCheckAuthRedirect()
   const userCardList = await getUserCardList(userId)
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-2">
       {userCardList ? (
-        userCardList.map((usercard: Card) => <CardWithDeleteForm key={usercard.id} card={usercard} />)
+        <div>
+          <h3>카드를 클릭하시면 결제를 진행하실 수 있습니다.</h3>
+          {userCardList.map((usercard: Card) => (
+            <CardWithDeleteForm key={usercard.id} card={usercard} />
+          ))}
+        </div>
       ) : (
-        <p>카드를 등록해주세요</p>
+        <p className="text-center">카드를 등록해주세요</p>
       )}
+      <CardWithDeleteForm key={card1.id} card={card1} />
+      <CardWithDeleteForm key={card2.id} card={card2} />
+      <CardWithDeleteForm key={card3.id} card={card3} />
     </div>
   )
 }
