ea9f57e7bfc7ceee4e4a14736813361abc9aeaf7
Wontae Choi <>
2024-03-31 19:54:31 +0900

    [Fix] Button 컴포넌트에 패딩이 들어가 있어서 카카오로그인 버튼에 패딩이 있었던 점 기본 button jsx로 교체

--- src/app/(noneauthheader)/auth/signin/page.tsx
+++ src/app/(noneauthheader)/auth/signin/page.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image"
 import { useSearchParams } from "next/navigation"
 import { signIn } from "next-auth/react"
-import Button from "@/components/molecules/Button/Button"
 import Title from "@/components/molecules/Title"
 export default function KakaoLoginPage() {
   return (
@@ -32,13 +31,13 @@ const KakaoLoginButtonSection = () => {
         <h3 className="text-center">궁극의 창작공간에서는 편하게 카카오톡 로그인을 지원해요!</h3>
         <div className="h-24"></div>
         <div className="flex justify-center">
-          <Button
+          <button
             onClick={() => {
               signIn("kakao", { callbackUrl: redirectUrl })
             }}
           >
             <Image src="/kakao/90x45.png" width={90} height={45} alt="카카오 로그인"></Image>
-          </Button>
+          </button>
         </div>
       </div>
       <div className="h-6"></div>
