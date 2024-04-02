af1ba66a7338a2487a48ead4aabda7b0d141c83c
Wontae Choi <>
2024-04-02 23:12:20 +0900

    [Add] BottomSheetButton에 form option 추가

--- src/components/molecules/Button/BottomSheetButton.tsx
+++ src/components/molecules/Button/BottomSheetButton.tsx
@@ -1,11 +1,13 @@
 type BottomSheetButtonProps = {
+  form?: boolean
   onClick?: () => void
   children: React.ReactNode
 }
 
-function BottomSheetButton({ onClick, children }: BottomSheetButtonProps) {
+function BottomSheetButton({ onClick, children, form = false }: BottomSheetButtonProps) {
+  const type = form ? "submit" : "button"
   return (
-    <button onClick={onClick} className="rounded-full bg-white-100 px-10 py-2 font-bold text-black-700">
+    <button type={type} onClick={onClick} className="rounded-full bg-white-100 px-10 py-2 font-bold text-black-700">
       {children}
     </button>
   )
