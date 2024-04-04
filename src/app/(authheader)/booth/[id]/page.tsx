514ccbc49838bccf9795faeab4a4ea54a15b2a4a
Wontae Choi <>
2024-04-04 17:16:22 +0900

    [Add] QR 좌석 booth UI 개선

--- src/app/(authheader)/booth/[id]/page.tsx
+++ src/app/(authheader)/booth/[id]/page.tsx
@@ -4,31 +4,26 @@ import { getBoookableById, getUserActivatedBoookable } from "@/actions/booth"
 import BookableCard from "@/components/molecules/BookableCard"
 import Button from "@/components/molecules/Button/Button"
 import SubTitle from "@/components/molecules/Title/SubTitle"
-import { Bookable } from "@/models/bookable"
+import Title from "@/components/molecules/Title/Title"
 
 export default async function BookablePage({ params }: { params: { id: string } }) {
   const { id } = params // QR코드에서 들어옴
   const userId = await getUserIdAfterCheckAuthRedirect()
   const currentBookable = await getBoookableById(id)
-  if (currentBookable.ticketId) {
-    return (
-      <div className="flex flex-col items-center justify-center">
+  const [userActivatedBookable] = await getUserActivatedBoookable(userId, [currentBookable.bookableType.type])
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Title text="좌석 QR코드" />
+      {currentBookable.ticketId ? (
         <SubTitle bold={true} text="이미 사용되고 있는 자리입니다." />
-      </div>
-    )
-  } else {
-    const userActivatedBookableList = await getUserActivatedBoookable(userId, [currentBookable.bookableType.type])
-    if (userActivatedBookableList) {
-      return (
-        <div className="flex flex-col items-center justify-center">
+      ) : userActivatedBookable ? (
+        <div className="flex flex-col items-center justify-center gap-2">
           <p>현재 사용중인 {currentBookable.bookableType.name}이 있습니다.</p>
-          {userActivatedBookableList.map((activatedBookable: Bookable) => (
-            <div key={activatedBookable.id}>
-              <BookableCard bookable={activatedBookable} />
-            </div>
-          ))}
+          <div key={userActivatedBookable.id}>
+            <BookableCard bookable={userActivatedBookable} />
+          </div>
           {["seat", "meetingroom"].includes(currentBookable.bookableType.type) ? (
-            <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center gap-2">
               <p>새로운 {currentBookable.bookableType.name}으로 이동하시겠습니까?</p>
               <BookableCard bookable={currentBookable} />
               <Link href={`/change/move/?id=${id}`}>
@@ -39,17 +34,15 @@ export default async function BookablePage({ params }: { params: { id: string } 
             <></>
           )}
         </div>
-      )
-    } else {
-      return (
-        <div className="flex flex-col items-center justify-center">
-          <SubTitle text="현재 사용중인 좌석이 없습니다. 티켓을 사용하시면 좌석을 사용할 수 있습니다." />
+      ) : (
+        <div className="flex flex-col items-center justify-center gap-2">
+          <SubTitle text="현재 좌석은 비어있습니다. 티켓을 사용하시면 좌석을 사용할 수 있습니다." />
           <SubTitle text="티켓을 사용하시겠습니까?" />
           <Link href={`/user/ticket/use/${currentBookable.bookableType.name}`}>
             <Button>티켓사용</Button>
           </Link>
         </div>
-      )
-    }
-  }
+      )}
+    </div>
+  )
 }
