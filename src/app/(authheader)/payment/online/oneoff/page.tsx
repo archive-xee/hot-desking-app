d77f537414c783dd6967826242e53b37235cff29
Wontae Choi <>
2024-04-05 15:57:39 +0900

    [Add] OrderCard 완성 & 사용되는 일회권 온라인 결제 페이지에 사용 (정기권 온라인 결제페이지에는 추후에)

--- src/app/(authheader)/payment/online/oneoff/page.tsx
+++ src/app/(authheader)/payment/online/oneoff/page.tsx
@@ -1,29 +1,38 @@
+import { headers } from "next/headers"
 import Link from "next/link"
 import Button from "@/components/molecules/Button/Button"
+import SubTitle from "@/components/molecules/Title/SubTitle"
 import Title from "@/components/molecules/Title/Title"
+import BorderCardContainer from "@/components/organisms/BorderCardContainer"
 import UserCardList from "@/components/organisms/Card/UserCardList"
 import NicepayPopupButton from "@/components/organisms/NicepayPopupButton"
+import OrderCard from "@/components/organisms/OrderCard"
 import { order1 } from "@/models/order"
-// import { useSearchParams } from "next/navigation"
-// import { useState, useEffect } from "react"
 
 // 여기도 Order정보 뿌려야 함
 
-export default function PaymentOnlineOneoffPage() {
-  // const searchParams = useSearchParams()
-  // const ticketId = searchParams.get("ticketId")
-  // 티켓id를 path에서 받을 것
+export default async function PaymentOnlineOneoffPage() {
+  const urlObject = new URL(headers().get("x-url")!)
+  const selectedTicketId = urlObject.searchParams.get("ticketId") ?? "ticketIdError"
 
   return (
     <div className="flex flex-col gap-2">
       <Title text="일회권 온라인 결제" />
-      <UserCardList />
-      <div className="flex flex-row justify-end gap-1">
-        <Link href="/card/register">
-          <Button>카드 등록</Button>
-        </Link>
-        <NicepayPopupButton order={order1} />
-      </div>
+      <SubTitle text="해당 티켓에 쓸 수 있는 쿠폰 선택하기" />
+      <BorderCardContainer>
+        <SubTitle bold={true} text="현재 결제 정보" />
+        <OrderCard ticketId={selectedTicketId} couponId={"12345"} />
+      </BorderCardContainer>
+      <BorderCardContainer>
+        <SubTitle bold={true} text="내 카드 목록" />
+        <UserCardList />
+        <div className="flex flex-row justify-end gap-1">
+          <Link href="/card/register">
+            <Button>카드 등록</Button>
+          </Link>
+          <NicepayPopupButton order={order1} />
+        </div>
+      </BorderCardContainer>
       결제이후 /user/ticket/use/(bookableType)으로 리디렉션 결제실패시에는 실패 페이지로
     </div>
   )
---
+++ src/components/organisms/OrderCard.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link"
+import Button from "../molecules/Button/Button"
+import StretchedTicket from "../molecules/Ticket/StretchedTicket"
+import { getCouponFrame } from "@/actions/couponframe"
+import { getTicketFrameById } from "@/actions/ticketframe"
+import StretchedCoupon from "@/components/molecules/Coupon/StretchedCoupon"
+import SubTitle from "@/components/molecules/Title/SubTitle"
+import { TicketFrame } from "@/models/ticket"
+
+export default async function OrderCard(props: { ticketId: string; couponId: string | null }) {
+  const { ticketId, couponId } = props
+  const ticketFrame: TicketFrame = await getTicketFrameById(ticketId)
+  let { number: ticketNumber, purchasePrice: ticketPrice } = ticketFrame
+  const { typeName } = ticketFrame
+  if (couponId) {
+    console.log(couponId)
+    const couponFrame = await getCouponFrame(couponId)
+    const { type, number: couponNumber } = couponFrame
+    switch (type) {
+      case "timebonus":
+        ticketNumber += couponNumber
+        break
+      case "discount":
+        ticketPrice -= couponNumber
+        break
+      default: // sale
+        ticketPrice -= (ticketPrice * couponNumber) / 100
+    }
+
+    return (
+      <div className="flex flex-col gap-2">
+        <div>
+          <SubTitle bold={true} text="선택된 티켓" />
+          <StretchedTicket ticketFrame={ticketFrame} />
+        </div>
+        <div>
+          <SubTitle bold={true} text="선택된 쿠폰" />
+          <StretchedCoupon couponFrame={couponFrame} />
+        </div>
+        <div>
+          <SubTitle bold={true} text={`${ticketNumber}${typeName} ${ticketPrice}원`} />
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div className="flex flex-col gap-2">
+        <SubTitle bold={true} text="선택된 티켓" />
+        <StretchedTicket ticketFrame={ticketFrame} />
+        <div className="flex flex-row justify-end">
+          <Link href={`/user/coupon/applicable?ticketId=${ticketId}`}>
+            <Button>적용가능한 쿠폰 찾아보기</Button>
+          </Link>
+        </div>
+        <SubTitle bold={true} text={`최종: ${ticketNumber}${typeName} ${ticketPrice}원`} />
+      </div>
+    )
+  }
+}
