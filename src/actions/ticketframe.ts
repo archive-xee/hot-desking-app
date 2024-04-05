9d977fc80a03e3b14e6523e98ef11f1f273a7a71
Wontae Choi <>
2024-04-05 15:55:33 +0900

    [Add] getTicketFrameById 쿼리 추가

--- src/actions/ticketframe.ts
+++ src/actions/ticketframe.ts
@@ -1,14 +1,45 @@
 "use server"
+import { TypedDocumentNode, gql } from "@apollo/client"
 import request from "graphql-request"
 import { APOLLO_ROUTER_URL } from "@/constant/graphql"
 import { GET_ALL_TICKET_FRAME } from "@/gql/ticketframe"
 import { TicketFrame } from "@/models/ticket"
 
-export async function getAllTicketFrame(type: TicketFrame["type"]) {
+export async function getAllTicketFrame(type?: TicketFrame["type"]) {
   const data = await request(APOLLO_ROUTER_URL, GET_ALL_TICKET_FRAME, {
     type,
   })
 
   const { ticketType: ticketFrameList } = data
   return ticketFrameList
+}
+
+export async function getTicketFrameById(id: string) {
+  const GET_TICKET_FRAME_BY_ID: TypedDocumentNode = gql`
+    query GetTicketFrameById($id: String) {
+      ticketType(ticketTypeId: $id) {
+        bookableType {
+          name
+          type
+        }
+        name
+        remaining
+        purchasePrice
+        expires
+        number
+        id
+        limit
+        type
+        typeName
+      }
+    }
+  `
+
+  const data = await request(APOLLO_ROUTER_URL, GET_TICKET_FRAME_BY_ID, {
+    id,
+  })
+
+  const { ticketType: ticketFrameList } = data
+  const [ticketFrame]: [TicketFrame] = ticketFrameList
+  return ticketFrame
 }
