24dc31d7c370ab2e99248a89fdd82c20959abd78
Wontae Choi <>
2024-04-06 12:25:15 +0900

    [Add] OrderCard에 쿠폰 변경가능한 옵션 부여

--- src/components/organisms/OrderCard.tsx
+++ src/components/organisms/OrderCard.tsx
@@ -13,7 +13,6 @@ export default async function OrderCard(props: { ticketId: string; couponId: str
   let { number: ticketNumber, purchasePrice: ticketPrice } = ticketFrame
   const { typeName } = ticketFrame
   if (couponId) {
-    console.log(couponId)
     const couponFrame = await getCouponFrame(couponId)
     const { type, number: couponNumber } = couponFrame
     switch (type) {
@@ -37,8 +36,13 @@ export default async function OrderCard(props: { ticketId: string; couponId: str
           <SubTitle bold={true} text="선택된 쿠폰" />
           <StretchedCoupon couponFrame={couponFrame} />
         </div>
-        <div>
-          <SubTitle bold={true} text={`${ticketNumber}${typeName} ${ticketPrice}원`} />
+        <div className="flex flex-row justify-end">
+          <Link href={`/user/coupon/applicable?ticketId=${ticketId}`}>
+            <Button>다른 쿠폰 적용해보기</Button>
+          </Link>
+        </div>
+        <div className="flex flex-row justify-end">
+          <SubTitle bold={true} text={`최종: ${ticketNumber}${typeName} ${ticketPrice}원`} />
         </div>
       </div>
     )
@@ -52,7 +56,9 @@ export default async function OrderCard(props: { ticketId: string; couponId: str
             <Button>적용가능한 쿠폰 찾아보기</Button>
           </Link>
         </div>
-        <SubTitle bold={true} text={`최종: ${ticketNumber}${typeName} ${ticketPrice}원`} />
+        <div className="flex flex-row justify-end">
+          <SubTitle bold={true} text={`최종: ${ticketNumber}${typeName} ${ticketPrice}원`} />
+        </div>
       </div>
     )
   }
